8cc6a84d3d7702cba8fa04022988a77cb2a4b420
Nour Domani <138979883+NourDomani@users.noreply.github.com>
2023-11-26 19:48:47 +0300

    Update loop.py
    watch where you press enter after run it

--- loop.py
+++ loop.py
@@ -2,16 +2,16 @@ import time
 import random
 import pyautogui as pg
 
-animals = ("smart","beuty","I don't know",)
+animals = ("smart","beuty","nasser")
 
 time.sleep(15)
 
 
 
 
-for i in range(50):
+for i in range(100):
     anml = random.choice(animals)
-    pg.write("He is"+ anml)
+    pg.write("He is -"+ anml)
     pg.press("enter")
     
 
